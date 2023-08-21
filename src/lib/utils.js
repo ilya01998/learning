18c1ee045232215347d62834850f7f5afd0aef7e
Ilya <tbq.active@gmail.com>
2023-08-21 17:09:09 +0300

    fix

--- src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.arrayToStackAdapter = void 0;
-var stack_1 = require("./stack");
-function arrayToStackAdapter(array) {
-    return new stack_1.Stack(array);
-}
-exports.arrayToStackAdapter = arrayToStackAdapter;
